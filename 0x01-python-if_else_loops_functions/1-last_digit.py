90ab1984f56d36571ed6cd4c9e881303aef35e14
TheSaviourEking <saviour.eking@outlook.com>
2022-09-27 16:33:02 -0700

    HotFix -> pycodestyle fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -12,4 +12,4 @@ if last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 if last_digit < 6 and last_digit != 0:
     print(f"Last digit of {number} is {last_digit} \
-            and is less than 6 and not 0")
+and is less than 6 and not 0")
