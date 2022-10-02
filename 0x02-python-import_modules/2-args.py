212bc662756b777cd2fc406e5c994cc704df8d83
TheSaviourEking <saviour.eking@outlook.com>
2022-10-02 00:08:27 -0700

    add 2-args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+if  __name__ != "__main__":
+    exit()
+elif __name__ == "__main__":
+    number = 1
+    n_arg = len(argv)
+    argc = n_arg - 1
+
+    if n_arg == 1:
+        print("{} arguments.".format(argc))
+    elif n_arg == 2:
+        print("{} argument:".format(argc))
+    else:
+        print("{} arguments:".format(argc))
+
+    for arguments in argv[1:]:
+        print("{}: {}".format(number, arguments)),
+        number = number + 1
+
