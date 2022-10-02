f1044da12cf9815504b7d6f05df9a0ece04474ee
TheSaviourEking <saviour.eking@outlook.com>
2022-10-02 04:16:19 -0700

    add 6-print_matrix_integer & test -> 6-main

--- 0x03-python-data_structures/6-main.py
+++ 0x03-python-data_structures/6-main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
+print_matrix_integer = __import__('6-print_matrix_integer')\
+        .print_matrix_integer
 
 matrix = [
     [1, 2, 3],
