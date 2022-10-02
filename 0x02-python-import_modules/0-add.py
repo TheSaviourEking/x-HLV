e0c100df1c2eb05a8e83eb5162ce43b183f57749
TheSaviourEking <saviour.eking@outlook.com>
2022-10-01 22:20:22 -0700

    add 0-add and dependencies

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+
 from add_0 import add
 
-if name == "__main__":
+if __name__ == "__main__":
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    c = add(a, b)
 
-
+    print("{:d} + {:d} = {:d}".format(a, b, c))
