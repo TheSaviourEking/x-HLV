d5eac7b0a9901ee400990161e08fc5d3af5e41f8
TheSaviourEking <saviour.eking@outlook.com>
2022-10-02 03:16:51 -0700

    add 5-no_c & test: 5-main

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_string = ""
-    new_string = my_string.translate({ord(i): None for i in "cC"})    
+    new_string = my_string.translate({ord(i): None for i in "cC"})
     return new_string
