cc6f6f7383fb85242849c58ed9c4880bf0f5a3a1
TheSaviourEking <saviour.eking@outlook.com>
2022-10-09 23:18:21 -0700

    add 3-common_elements and test:>3-main.py

---
+++ 0x04-python-more_data_structures/3-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+n_elements = __import__('3-common_elements').common_elements
+
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+c_set = common_elements(set_1, set_2)
+print(sorted(list(c_set)))
