6e2afe2fd1ee63495a288d4ee7945b2298e4aa04
Saviour Eking <106761645+TheSaviourEking@users.noreply.github.com>
2022-10-14 04:45:51 +0000

    Fri, Oct 14, 2022, 4:45 AM +00:00

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    """
+    prints a list of anything, but only prints the integers
+    Returns the amount of integers printed
+    """
     printed = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
             printed += 1
-        except:
-            continue
-    print()
+        except (TypeError, ValueError):
+            print()
     return printed
